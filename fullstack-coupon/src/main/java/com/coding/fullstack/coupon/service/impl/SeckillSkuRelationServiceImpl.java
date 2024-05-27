f6327b886ae9d20e4d6956be8e9fd1274d51e48d
emon <liming20110711@163.com>
2024-05-27 08:25:37 +0800

    update:20240527-02

--- fullstack-coupon/src/main/java/com/coding/fullstack/coupon/service/impl/SeckillSkuRelationServiceImpl.java
+++ fullstack-coupon/src/main/java/com/coding/fullstack/coupon/service/impl/SeckillSkuRelationServiceImpl.java
@@ -1,27 +1,35 @@
 package com.coding.fullstack.coupon.service.impl;
 
-import org.springframework.stereotype.Service;
 import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.coding.common.utils.PageUtils;
 import com.coding.common.utils.Query;
-
 import com.coding.fullstack.coupon.dao.SeckillSkuRelationDao;
 import com.coding.fullstack.coupon.entity.SeckillSkuRelationEntity;
 import com.coding.fullstack.coupon.service.SeckillSkuRelationService;
 
-
 @Service("seckillSkuRelationService")
-public class SeckillSkuRelationServiceImpl extends ServiceImpl<SeckillSkuRelationDao, SeckillSkuRelationEntity> implements SeckillSkuRelationService {
+public class SeckillSkuRelationServiceImpl extends ServiceImpl<SeckillSkuRelationDao, SeckillSkuRelationEntity>
+    implements SeckillSkuRelationService {
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
-        IPage<SeckillSkuRelationEntity> page = this.page(
-                new Query<SeckillSkuRelationEntity>().getPage(params),
-                new QueryWrapper<SeckillSkuRelationEntity>()
-        );
+
+        String promotionSessionId = (String)params.get("promotionSessionId");
+
+        LambdaQueryWrapper<SeckillSkuRelationEntity> lambdaQuery = Wrappers.lambdaQuery(SeckillSkuRelationEntity.class);
+        lambdaQuery.eq(StringUtils.isNotEmpty(promotionSessionId), SeckillSkuRelationEntity::getPromotionSessionId,
+            promotionSessionId);
+
+        IPage<SeckillSkuRelationEntity> page =
+            this.page(new Query<SeckillSkuRelationEntity>().getPage(params), lambdaQuery);
 
         return new PageUtils(page);
     }
